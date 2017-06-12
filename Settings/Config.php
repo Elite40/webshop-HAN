0828a633b2559cc91555c6c7255d4225d6709388
o.kaya <o.kaya@beslist.nl>
2017-06-12 19:25:15 +0200

    Leaving it as it is...

---
+++ Settings/Config.php
@@ -0,0 +1,17 @@
+<?php
+class Config
+{
+    public static $config = [
+        'host' => 'localhost',
+        'port' => '8889',
+        'name' => 'webshop',
+        'username' => 'root',
+        'password' => 'root',
+        'charset' => 'utf8'
+    ];
+    public static function getConfig()
+    {
+        return self::$config;
+    }
+}
+
