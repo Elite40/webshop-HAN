e31875aec8945ae17649fdbf1685d4f208778b1a
o.kaya <o.kaya@beslist.nl>
2017-06-29 21:34:22 +0200

    Added a link to 'Winkelwagen' text

--- views/loggedIn.php
+++ views/loggedIn.php
@@ -5,5 +5,9 @@
 
         <input type="submit" value="Uitloggen">
     </form>
-    Winkelwagen <img src="/webshop-HAN/assets/img/shopping-cart.png"><span class="shoppingcart-amount"> ( <?php echo count($_SESSION['cart_items']);?>)</span>
+    <a href="?page=winkelwagen">Winkelwagen</a>
+    <img src="/webshop-HAN/assets/img/shopping-cart.png"><span
+            class="shoppingcart-amount">
+        ( <?php echo count($_SESSION['cart_items']); ?>)
+    </span>
 </div>
