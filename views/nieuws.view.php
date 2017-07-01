a3edab7f960ece6a289efc63b2151a2d4cf5314b
o.kaya <o.kaya@beslist.nl>
2017-07-01 23:39:47 +0200

    vacatures en nieuws pagina toegevoegd

--- views/nieuws.view.php
+++ views/nieuws.view.php
@@ -1,1 +1,55 @@
-<h2>Momenteel geen nieuws te vinden!</h2>
+<div class="news-container">
+
+    <div class="news-item">
+
+        <div class="headline">
+            <h1 class="news-title">The paradis on earth: unknown place</h1>
+            <span class="published-date">Geplaatst op 12-02-2015</span>
+        </div>
+
+
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+        Ab alias architecto deserunt distinctio maxime molestias placeat quas quod repellat ut.
+        Commodi ducimus fugiat inventore! Eum facere molestias nemo neque odio.
+
+        <div class="read-more-btn-container">
+            <a href="#" class="shop-button">Lees meer</a>
+        </div>
+    </div>
+
+    <div class="news-item">
+
+        <div class="headline">
+            <h1 class="news-title">PHP is awesome!</h1>
+            <span class="published-date">Geplaatst op 27-06-2017</span>
+        </div>
+
+
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+        Ab alias architecto deserunt distinctio maxime molestias placeat quas quod repellat ut.
+        Commodi ducimus fugiat inventore! Eum facere molestias nemo neque odio.
+
+        <div class="read-more-btn-container">
+            <a href="#" class="shop-button">Lees meer</a>
+        </div>
+    </div>
+
+    <div class="news-item">
+
+        <div class="headline">
+            <h1 class="news-title">Some other fake news</h1>
+            <span class="published-date">Geplaatst op 12-02-2015</span>
+        </div>
+
+
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+        Ab alias architecto deserunt distinctio maxime molestias placeat quas quod repellat ut.
+        Ab alias architecto deserunt distinctio maxime molestias placeat quas quod repellat ut.
+        Commodi ducimus fugiat inventore! Eum facere molestias nemo neque odio.
+
+        <div class="read-more-btn-container">
+            <a href="#" class="shop-button">Lees meer</a>
+        </div>
+    </div>
+
+</div>
--- views/vacatures.view.php
+++ views/vacatures.view.php
@@ -1,1 +1,31 @@
-<h2>Geen vacatures beschikbaar.</h2>
+<div class="jobs-container">
+    <h1 class="jobs-title">Openstaande vacatures</h1>
+
+    <div class="job">
+        <a href="">
+            <h2 class="title">Php developer</h2>
+
+            <p>Full time (40)</p>
+        </a>
+
+    </div>
+
+    <div class="job">
+        <a href="">
+            <h2 class="title">Augmented Reality developer</h2>
+
+            <p>Parttime (32-40) </p>
+        </a>
+
+    </div>
+
+    <div class="job">
+        <a href="">
+            <h2 class="title">Front-end developer</h2>
+
+            <p>Parttime (32-40) </p>
+        </a>
+    </div>
+</div>
+
+
